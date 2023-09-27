f11e6b8835e3402ee71634a56e83473229460ca0
kaami419 <tareen544@gmail.com>
2023-09-27 16:16:05 +0500

    portfolio competed

---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1D0270",
+        secondary: "white",
+        tertiary: "#151030",
+        "black-100": "#191040",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/latestbg.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
